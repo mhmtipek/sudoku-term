21a4b5fbfec193190d3546c3b8a040005159a4c4
Mehmet <mehmetipek256@icloud.com>
2025-04-05 02:24:59 +0200

    main: create board in thread, play animation

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
-#![allow(non_snake_case)]
-
 use std::{
     io,
     time::{Duration, Instant},
 };
 //use std::fmt;
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 use ratatui::{
     crossterm::event::{self, KeyCode, KeyEventKind},
     layout::Rect,
@@ -14,28 +12,39 @@ use ratatui::{
     widgets::{Cell, Row, Table, TableState},
     DefaultTerminal,
 };
+use std::thread;
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum, Debug)]
+enum Difficulty {
+    // Easy
+    Easy,
+    // Medium
+    Medium,
+    // Hard
+    Hard,
+}
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     /// Difficulty
-    #[arg(short, long, default_value_t = 4)]
-    difficulty: u8,
+    #[arg(value_enum, default_value_t = Difficulty::Medium)]
+    difficulty: Difficulty,
 
     /// Show elapsed time
-    #[arg(short, long)]
-    showElapsedTime: bool,
+    #[arg(short, long, default_value_t = true)]
+    show_elapsed_time: bool,
 }
 
 struct Board {
     rows: [[u8; 9]; 9],
     table_state: TableState,
     current_cell: (u8, u8),
-    difficulty: u8,
+    difficulty: Difficulty,
 }
 
 impl<'a> Board {
-    fn new(difficulty: u8) -> Self {
+    fn new(difficulty: Difficulty) -> Self {
         let current_cell = (0u8, 0u8);
         let mut table_state = TableState::default();
         table_state.select_cell(Some((0, 0)));
@@ -85,8 +94,8 @@ impl<'a> Board {
         Table::new(rows, widths)
             .column_spacing(0)
             .bg(Color::Indexed(0))
-            .row_highlight_style(Style::new().fg(Color::Indexed(210)))
-            .column_highlight_style(Style::new().fg(Color::Indexed(210)))
+            .row_highlight_style(Style::new().fg(Color::Indexed(230)))
+            .column_highlight_style(Style::new().fg(Color::Indexed(230)))
             .cell_highlight_style(Style::new().fg(Color::Indexed(120)))
     }
 
@@ -103,21 +112,62 @@ impl<'a> Board {
     }
 }
 
+fn generate_initial_board(difficulty: Difficulty) -> [[u8; 9]; 9] {
+    // Assign initial values. Maybe create available_values function
+    // Solve board. Create solve function.
+    // Remove cells until desired difficulty is reached.
+    thread::sleep(Duration::from_millis(3000));
+    let mut board: [[u8; 9]; 9] = [[3; 9]; 9];
+    board
+}
+
 fn main() -> io::Result<()> {
     let args = Args::parse();
 
     let mut terminal = ratatui::init();
     terminal.clear()?;
 
-    let app_result = run(terminal, args.difficulty, args.showElapsedTime);
+    let app_result = run(terminal, args.difficulty, args.show_elapsed_time);
     ratatui::restore();
     app_result
 }
 
-fn run(mut terminal: DefaultTerminal, difficulty: u8, showElapsedTime: bool) -> io::Result<()> {
+fn run(
+    mut terminal: DefaultTerminal,
+    difficulty: Difficulty,
+    show_elapsed_time: bool,
+) -> io::Result<()> {
     let mut board: Board = Board::new(difficulty);
-    let start_time = Instant::now();
 
+    // Start the thread which creates the initial board here.
+    let init_thread_handle = thread::spawn(move || generate_initial_board(difficulty));
+
+    // The loop until initial board is created
+    let mut counter = 0;
+    loop {
+        if init_thread_handle.is_finished() {
+            break;
+        }
+
+        // Animate text
+        let text = "Generating board ... ";
+        let shift = counter % text.len();
+        let print_text = format!("{}{}", &text[shift..], &text[0..shift]);
+
+        terminal.draw(|frame| {
+            frame.render_widget(
+                Text::from(print_text).centered(),
+                Rect::new(0, frame.area().height / 2, frame.area().width, 1),
+            );
+        })?;
+
+        thread::sleep(Duration::from_millis(100));
+        counter = counter + 1;
+    }
+    board.rows = init_thread_handle.join().unwrap();
+
+    // The game loop
+    let start_time = Instant::now();
     loop {
         terminal.draw(|frame| {
             let board_rect = Rect::new(
@@ -127,7 +177,7 @@ fn run(mut terminal: DefaultTerminal, difficulty: u8, showElapsedTime: bool) -> 
                 9,
             );
             frame.render_stateful_widget(board.create_table(), board_rect, &mut board.table_state);
-            if showElapsedTime {
+            if show_elapsed_time {
                 let time_label =
                     Text::from(format!("{} secs", start_time.elapsed().as_secs())).right_aligned();
                 frame.render_widget(
@@ -140,7 +190,7 @@ fn run(mut terminal: DefaultTerminal, difficulty: u8, showElapsedTime: bool) -> 
                     ),
                 );
             }
-            let difficulty_label = Text::from(format!("{}", board.difficulty)).left_aligned();
+            let difficulty_label = Text::from(format!("{:?}", board.difficulty)).left_aligned();
             frame.render_widget(
                 difficulty_label,
                 Rect::new(0, frame.area().height - 1, frame.area().width / 2, 1),
